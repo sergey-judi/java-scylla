b63585d8cdd0c2bbcdbc7c8409d402051a454629
sergey-judi <serega7qwerty@gmail.com>
2024-02-01 19:53:02 +0200

    Tuned CqlSession configuration -> switched from using default config to build result

--- src/main/java/org/example/config/ScyllaSessionConfiguration.java
+++ src/main/java/org/example/config/ScyllaSessionConfiguration.java
@@ -2,9 +2,7 @@ package org.example.config;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
-import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import org.cognitor.cassandra.migration.spring.CassandraMigrationAutoConfiguration;
-import org.example.JavaScyllaApplication;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +13,8 @@ public class ScyllaSessionConfiguration {
 
   @Bean
   @Qualifier(CassandraMigrationAutoConfiguration.CQL_SESSION_BEAN_NAME)
-  public CqlSession cassandraMigrationCqlSession() {
-    DriverConfigLoader driverConfigLoader = DriverConfigLoader.fromDefaults(
-        JavaScyllaApplication.class.getClassLoader()
-    );
-
-    return CqlSession.builder()
-        .withConfigLoader(driverConfigLoader)
-        .build();
+  public CqlSession cassandraMigrationCqlSession(CqlSessionBuilder cqlSessionBuilder) {
+    return cqlSessionBuilder.build();
   }
 
   @Bean
